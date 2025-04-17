ba96fb6d19912011e8a24b92c2d9ce27cd79dcf2
tsuki <wt041099@163.com>
2025-04-17 18:42:31 +0800

    pubble_sort

--- src/main/java/org/example/pubble_sort/sort.java
+++ src/main/java/org/example/pubble_sort/sort.java
@@ -1,4 +1,23 @@
 package org.example.pubble_sort;
 
+/*
+ * 冒泡排序*/
 public class sort {
+    void pubbleSort(int[] c) {
+
+        for (int j = c.length - 1; j > 0; j--) {//轮最大长度-1次，不算0，每伦比上一次-1
+            boolean flag = false;//初始化flag
+            for (int i = 0; i < c.length - 1; i++) {//当前面的数大就交换,比较最大长度-1次，算上0次则-2
+                if (c[i] > c[i + 1]) {
+                    int temp = c[i];
+                    c[i] = c[i + 1];
+                    c[i + 1] = temp;
+                    flag = true;//实现交换变成true
+                }
+            }
+            if (!flag) {
+                break;//如果flag为false则该轮没有实现交换，数组已经有序
+            }
+        }
+    }
 }
--- src/main/java/org/example/pubble_sort/test.java
+++ src/main/java/org/example/pubble_sort/test.java
@@ -1,4 +1,12 @@
 package org.example.pubble_sort;
 
+import java.util.Arrays;
+
 public class test {
+    public static void main(String[] args) {
+        int[] c = { 5, 4, 3, 2, 1 };
+        sort q = new sort();
+        q.pubbleSort(c);
+        System.out.println(Arrays.toString(c));
+    }
 }
